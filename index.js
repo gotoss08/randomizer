10284110444b3287af78385dce00b566fbc4594f
gotoss08 <gotoss08@gmail.com>
2018-05-03 21:19:11 +0600

    changed port to use default if available

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const server = http.createServer(app);
 
 const io = require('socket.io')(server);
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -48,4 +50,4 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('listening on port 3000'));
+server.listen(port, () => console.log('listening on port 3000'));
