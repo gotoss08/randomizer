66c00bf030f1b4d59ef8620d80b435c90b1f44e7
gotoss08 <gotoss08@gmail.com>
2018-05-03 21:23:28 +0600

    removed junk from index.html

--- index.js
+++ index.js
@@ -50,4 +50,4 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(port, () => console.log('listening on port 3000'));
+server.listen(port, () => console.log('listening on port ' + port));
