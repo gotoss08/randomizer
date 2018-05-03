21cd7fc84318fc42c957d763031b89a0a293af95
gotoss08 <gotoss08@gmail.com>
2018-05-03 21:07:56 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const uniqid = require('uniqid');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    return res.sendFile('index.html');
+});
+
+let items = [];
+
+io.on('connection', (socket) => {
+    console.log('Socket connected');
+
+    socket.on('disconnect', () => {
+        console.log('Socket disconnected');
+    });
+
+    socket.on('add', (text) => {
+        const item = {
+            id: uniqid(),
+            text,
+        };
+        items.push(item);
+        io.emit('update', items);
+    });
+
+    socket.on('remove', (id) => {
+        items = items.filter((item) => item.id != id);
+        io.emit('update', items);
+    });
+
+    socket.on('randomize', () => {
+        const randomIndex = Math.round(Math.random() * (items.length - 1));
+        let id = items[randomIndex].id;
+        io.emit('randomize', id);
+    });
+});
+
+server.listen(3000, () => console.log('listening on port 3000'));
---
+++ public/index.js
@@ -0,0 +1,64 @@
+const socket = io();
+
+const items = $('.items');
+const input = $('#add-new-input');
+const button = $('#add-new-button');
+const itemTemplate = $($('#item-template').html());
+
+const sendAddItemRequest = function sendAddItemRequest() {
+    const name = input.val().trim();
+    if (name) socket.emit('add', name);
+    input.val('');
+    input.focus();
+};
+
+const sendRemoveItemRequest = function sendAddItemRequest(id) {
+    socket.emit('remove', id);
+};
+
+const sendRandomizeRequest = function sendRandomizeItemsRequest() {
+    socket.emit('randomize');
+};
+
+const addNewItem = function addNewItem(text, id) {
+    let item = itemTemplate.clone();
+    item.attr('id', id);
+    item.find('.item-text').text(text);
+    item.find('.item-button').click(() => {
+        sendRemoveItemRequest(id);
+    });
+    items.append(item);
+    items.children().removeClass('result result-other');
+};
+
+const removeItem = function removeItem(id) {
+    const itemToRemove = $(`#${id}`);
+    itemToRemove.remove();
+    items.children().removeClass('result result-other');
+};
+
+const randomize = function randomize(id) {
+    items.children().removeClass('result').addClass('result-other');
+    let randomItem = $(`#${id}`);
+    randomItem.removeClass('result-other').addClass('result');
+};
+
+button.click(() => sendAddItemRequest());
+
+input.keypress((e) => {
+    if (e.which == 13) sendAddItemRequest();
+});
+
+let randomizeButton = $('.randomize');
+randomizeButton.click(() => {
+    sendRandomizeRequest();
+});
+
+socket.on('update', (_items) => {
+    items.empty();
+    _items.forEach((item) => {
+        addNewItem(item.text, item.id);
+    });
+});
+
+socket.on('randomize', (id) => randomize(id));
